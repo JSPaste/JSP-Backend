d92c47121c1956ab46f845e1cccd758fadb9850a
Ivan Gabaldon <git@inetol.net>
2024-11-22 00:47:00 +0100

    update license information

--- src/server/documentation.ts
+++ src/server/documentation.ts
@@ -12,7 +12,7 @@ export const documentation = (instance: OpenAPIHono): void => {
 			description: `Note: The latest API version can be accessed with "${config.apiPath}/documents" alias route.`,
 			license: {
 				name: 'EUPL-1.2',
-				url: 'https://joinup.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt'
+				url: 'https://eur-lex.europa.eu/eli/dec_impl/2017/863'
 			}
 		},
 		servers: [
