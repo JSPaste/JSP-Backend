c11008dc09f107722ea387dc546d05e3531400f8
Ivan Gabaldon <git@inetol.net>
2024-11-24 16:18:24 +0100

    improve compression ratio

--- src/document/compression.ts
+++ src/document/compression.ts
@@ -1,8 +1,14 @@
 import { type InputType, brotliCompressSync, brotliDecompressSync } from 'node:zlib';
+import { constants as zlibConstants } from 'zlib';
 
 export const compression = {
 	encode: (data: InputType): Buffer => {
-		return brotliCompressSync(data);
+		return brotliCompressSync(data, {
+			params: {
+				[zlibConstants.BROTLI_PARAM_QUALITY]: 1,
+				[zlibConstants.BROTLI_PARAM_LGWIN]: 24
+			}
+		});
 	},
 
 	decode: (data: InputType): Buffer => {
